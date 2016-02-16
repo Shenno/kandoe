9ac88a3d5208bff915bbbd4f533851f8029e707d
Blaperile <Devi.Bral@telenet.be>
2016-02-16 11:57:31 +0100

    Empty test classes

---
+++ src/test/java/be/kdg/kandoe/integration/ITcontent.java
@@ -0,0 +1,14 @@
+package be.kdg.kandoe.integration;
+
+import org.junit.Before;
+
+/**
+ * @author Devi Bral
+ * @version 1.0 16/02/2016 11:54
+ */
+public class ITcontent {
+    @Before
+    public void setup() {
+
+    }
+}
---
+++ src/test/java/be/kdg/kandoe/integration/ITsession.java
@@ -0,0 +1,14 @@
+package be.kdg.kandoe.integration;
+
+import org.junit.Before;
+
+/**
+ * @author Devi Bral
+ * @version 1.0 16/02/2016 11:55
+ */
+public class ITsession {
+    @Before
+    public void setup() {
+
+    }
+}
