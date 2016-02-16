aeaae3e267334620c05a979e4850c0992cf35e29
Blaperile <Devi.Bral@telenet.be>
2016-02-16 11:59:14 +0100

    Empty test classes

--- src/test/java/be/kdg/kandoe/integration/ITsession.java
+++ src/test/java/be/kdg/kandoe/integration/ITsession.java
@@ -2,10 +2,7 @@ package be.kdg.kandoe.integration;
 
 import org.junit.Before;
 
-/**
- * @author Devi Bral
- * @version 1.0 16/02/2016 11:55
- */
+
 public class ITsession {
     @Before
     public void setup() {
