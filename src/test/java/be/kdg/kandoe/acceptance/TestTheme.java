d392ef583443bf3dee67a8e104504d228f84a324
Blaperile <Devi.Bral@telenet.be>
2016-02-21 22:45:26 +0100

    Release 2: user-stories 1-7,48

--- src/test/java/be/kdg/kandoe/acceptance/TestTheme.java
+++ src/test/java/be/kdg/kandoe/acceptance/TestTheme.java
@@ -125,5 +125,6 @@ public class TestTheme {
         assertNotNull(theme);
 
      //   contentService.addTheme(emailAdress, name, description, isCommentaryAllowed, isAddingAdmited, organisation, tags);
+     //
     }*/
 }
