e1cf8986a2a03cd08fdd981e8b6440b4184aa832
Blaperile <Devi.Bral@telenet.be>
2016-02-23 11:32:12 +0100

    Wijzigen thema: TypeScript component & HTML code - eerste versie

---
+++ src/main/webapp/app/components/editTheme.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+import {Router} from 'angular2/router';
+
+@Component({
+    selector: 'edit-Theme',
+    templateUrl: 'app/partials_html/editTheme.component.html'
+
+})
+export class EditThemeComponent {
+
+}
