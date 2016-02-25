38a38a5f952b728577e9b0c3001c07977d129219
admin <shenno.w@gmail.com>
2016-02-25 16:14:55 +0100

    angular

---
+++ src/main/webapp/app/components/test.component.js
@@ -0,0 +1,37 @@
+System.register(['angular2/core'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+        switch (arguments.length) {
+            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+        }
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var TestComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            TestComponent = (function () {
+                function TestComponent() {
+                }
+                TestComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-cirkelsessie',
+                        templateUrl: 'app/partials_html/cirkelsessie.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], TestComponent);
+                return TestComponent;
+            })();
+            exports_1("TestComponent", TestComponent);
+        }
+    }
+});
+//# sourceMappingURL=test.component.js.map
