fa8886596ed12be00b111ffdcd14f67649069825
admin <shenno.w@gmail.com>
2016-02-25 15:55:19 +0100

    ts problem

---
+++ src/main/webapp/app/components/createTheme.component.js
@@ -0,0 +1,37 @@
+System.register(['angular2/core'], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+        switch (arguments.length) {
+            case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+            case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+            case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+        }
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1;
+    var CreateThemeComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            CreateThemeComponent = (function () {
+                function CreateThemeComponent() {
+                }
+                CreateThemeComponent = __decorate([
+                    core_1.Component({
+                        selector: 'create-theme',
+                        templateUrl: 'app/partials_html/createTheme.component.html'
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], CreateThemeComponent);
+                return CreateThemeComponent;
+            })();
+            exports_1("CreateThemeComponent", CreateThemeComponent);
+        }
+    }
+});
+//# sourceMappingURL=createTheme.component.js.map
