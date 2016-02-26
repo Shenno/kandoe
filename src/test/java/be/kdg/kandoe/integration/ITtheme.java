55213535f3e0609ada581cdc59ede7c3931c7ff8
Blaperile <Devi.Bral@telenet.be>
2016-02-26 16:50:53 +0100

    Aanmaken thema: integratietest

--- src/test/java/be/kdg/kandoe/integration/ITtheme.java
+++ src/test/java/be/kdg/kandoe/integration/ITtheme.java
@@ -21,30 +21,47 @@ public class ITtheme {
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver.exe");
          WebDriver driver = new ChromeDriver();
         driver.get("http://127.0.0.1:9966/kandoe/#/createTheme");
+
         WebElement element = driver.findElement(By.id("app"));
         element = element.findElement(By.tagName("create-theme"));
+
         element = element.findElement(By.name("ib_name"));
         assertEquals("input",element.getTagName());
+        assertEquals("text", element.getAttribute("type"));
         element.sendKeys("themename");
+
         element = driver.findElement(By.name("ib_description"));
         assertEquals("input",element.getTagName());
+        assertEquals("text", element.getAttribute("type"));
         element.sendKeys("description");
+
         element = driver.findElement(By.name("cb_commentaryAllowed"));
         assertEquals("input",element.getTagName());
+        assertEquals("checkbox", element.getAttribute("type"));
         element.click();
+
         element = driver.findElement(By.name("cb_addingAdmitted"));
         assertEquals("input",element.getTagName());
+        assertEquals("checkbox", element.getAttribute("type"));
         element.click();
+
         // Select dropdown = new Select(driver.findElement(By.name("dd_organisation")));
         //dropdown.selectByVisibleText("organisatie");
-        element = driver.findElement(By.name("tags"));
+
+        element = driver.findElement(By.id("tags"));
         assertEquals("div",element.getTagName());
+
         element = driver.findElement(By.name("ib_newTag"));
         assertEquals("input",element.getTagName());
+        assertEquals("text", element.getAttribute("type"));
+
         element= driver.findElement(By.name("btn_addTag"));
         assertEquals("button",element.getTagName());
+        assertEquals("Voeg tag toe", element.getText());
+
         element= driver.findElement(By.name("btn_save"));
         assertEquals("button",element.getTagName());
+        assertEquals("Opslaan", element.getText());
         element.submit();
 
         (new WebDriverWait(driver, 10)).until((WebDriver d) -> d.getTitle().equals("Theme: themename"));
