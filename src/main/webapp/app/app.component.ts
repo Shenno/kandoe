9acecd2027adf98425cc4a367b786af3ed1b48fd
Blaperile <Devi.Bral@telenet.be>
2016-03-01 14:44:45 +0100

    Bugfix createOrganisation.ts (deel 2)

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -5,7 +5,8 @@ import {CreateThemeComponent} from "./components/createTheme.component";
 import {EditThemeComponent} from "./components/editTheme.component";
 import {bootstrap} from "angular2/bootstrap";
 import {DetailThemeComponent} from "./components/detailTheme.component";
-import {CreateOrganisationComponent} from "./components/createOrganisation.component.ts";
+import {CreateOrganisationComponent} from "./components/createOrganisation.component";
+//import {CreateOrganisationComponent} from "./components/createOrganisation.component.ts";
 
 @Component({
     selector: 'my-app',
@@ -19,7 +20,7 @@ import {CreateOrganisationComponent} from "./components/createOrganisation.compo
     {path:'/createTheme',name:'CreateTheme',component: CreateThemeComponent},
     {path:'/detailTheme/:themeId', name:'DetailTheme', component: DetailThemeComponent},
     {path:'/editTheme/:themeId',name:'EditTheme',component:EditThemeComponent},
-    {path:'/createOrganisation',name:'CreateOrganisation',component:CreateOrganisationComponent}
+    {path:'/createOrganisation',name:'CreateOrganisation',component: CreateOrganisationComponent}
     //{path:'/detailOrganisation',name:'DetailOrganisation',component:DetailOrganisationComponent}
 
 ])
