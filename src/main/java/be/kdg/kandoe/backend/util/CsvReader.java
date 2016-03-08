4d944b36456f52e2b9878f3bb762182bb11069ca
LaurensWolf <laurens.wolf@student.kdg.be>
2016-03-08 15:52:56 +0100

    csv reader fix

--- src/main/java/be/kdg/kandoe/backend/util/CsvReader.java
+++ src/main/java/be/kdg/kandoe/backend/util/CsvReader.java
@@ -1,7 +1,65 @@
 package be.kdg.kandoe.backend.util;
 
+import be.kdg.kandoe.backend.dom.content.Card;
+import be.kdg.kandoe.backend.dom.content.Tag;
+import be.kdg.kandoe.backend.dom.content.Theme;
+import be.kdg.kandoe.backend.dom.user.Organisation;
+import be.kdg.kandoe.backend.dom.user.User;
+import be.kdg.kandoe.backend.services.api.UserService;
+import be.kdg.kandoe.backend.services.exceptions.UserServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Len on 8-3-2016.
  */
 public class CsvReader {
-}
+
+
+        public ArrayList<Card> run(String csvFile, Theme theme) {
+            ArrayList<Card> cards = new ArrayList<>();
+
+            BufferedReader br = null;
+            String line = "";
+            String cvsSplitBy = ";";
+
+            try {
+
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+
+                    // use comma as separator
+                    String[] cardString = line.split(cvsSplitBy);
+                    Card newCard= new Card(cardString[0],cardString[1],theme);
+                    cards.add(newCard);
+
+
+
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            return cards;
+        }
+
+    }
+
