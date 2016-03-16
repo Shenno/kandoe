06d413d98996e51f5ff268d4c17ae4606ab4af5b
Blaperile <Devi.Bral@telenet.be>
2016-03-16 12:55:23 +0100

    Dashboard: Knop terug naar thema

--- src/main/webapp/app/components/dashboard.component.js
+++ src/main/webapp/app/components/dashboard.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', "angular2/common", "./apriori.component"], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', "angular2/common", "./apriori.component", "angular2/router"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,12 +8,15 @@ System.register(['angular2/core', "angular2/common", "./apriori.component"], fun
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1, common_2, apriori_component_1;
+    var core_1, router_1, common_1, common_2, apriori_component_1, router_2;
     var DashboardComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (router_1_1) {
+                router_1 = router_1_1;
             },
             function (common_1_1) {
                 common_1 = common_1_1;
@@ -21,11 +24,20 @@ System.register(['angular2/core', "angular2/common", "./apriori.component"], fun
             },
             function (apriori_component_1_1) {
                 apriori_component_1 = apriori_component_1_1;
+            },
+            function (router_2_1) {
+                router_2 = router_2_1;
             }],
         execute: function() {
             DashboardComponent = (function () {
-                function DashboardComponent() {
+                function DashboardComponent(router, routeParam) {
+                    this.router = router;
+                    this.routeParam = routeParam;
                 }
+                DashboardComponent.prototype.returnToTheme = function (event) {
+                    event.preventDefault();
+                    this.router.navigate(['/Theme', { themeId: this.routeParam.params["themeId"] }]);
+                };
                 DashboardComponent = __decorate([
                     core_1.Component({
                         selector: 'dashboard',
@@ -33,7 +45,7 @@ System.register(['angular2/core', "angular2/common", "./apriori.component"], fun
                         templateUrl: 'app/partials_html/dashboard.component.html',
                         encapsulation: core_1.ViewEncapsulation.None,
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [router_1.Router, router_2.RouteParams])
                 ], DashboardComponent);
                 return DashboardComponent;
             })();
--- src/main/webapp/app/components/dashboard.component.ts
+++ src/main/webapp/app/components/dashboard.component.ts
@@ -3,6 +3,7 @@ import {Router} from 'angular2/router';
 import {CORE_DIRECTIVES} from "angular2/common";
 import {FORM_DIRECTIVES} from "angular2/common";
 import {AprioriComponent} from "./apriori.component";
+import {RouteParams} from "angular2/router";
 
 @Component({
     selector: 'dashboard',
@@ -12,5 +13,17 @@ import {AprioriComponent} from "./apriori.component";
 })
 
 export class DashboardComponent {
-    
+
+    private router: Router;
+    private routeParam: RouteParams;
+
+    public constructor(router: Router, routeParam: RouteParams) {
+        this.router = router;
+        this.routeParam = routeParam;
+    }
+
+    public returnToTheme(event): void {
+        event.preventDefault();
+        this.router.navigate(['/Theme', {themeId: this.routeParam.params["themeId"]}]);
+    }
 }
