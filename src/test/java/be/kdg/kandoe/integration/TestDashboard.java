b2eafc999adeb6e863eae73208c1c179eb14cd30
Blaperile <Devi.Bral@telenet.be>
2016-03-20 12:01:48 +0100

    Vaakst voorkomende kaartjescombinaties: integratietest

--- src/test/java/be/kdg/kandoe/integration/TestDashboard.java
+++ src/test/java/be/kdg/kandoe/integration/TestDashboard.java
@@ -44,10 +44,16 @@ public class TestDashboard {
 
     @Before
     public void setup() {
-       // user = new User("firstname.lastname@kandoe.be", "password");
-       // user = userService.addUser(user);
+        user = new User("firstname.lastname@kandoe.be", "password");
+        user = userService.addUser(user);
 
-       /* organisation = new Organisation("organisation", user);
+        User user2 = new User("firstname2.lastname2@kandoe.be", "password");
+        user2 = userService.addUser(user2);
+
+        User user3 = new User("firstname3.lastname3@kandoe.be", "password");
+        user3 = userService.addUser(user3);
+
+        organisation = new Organisation("organisation", user);
         organisation = userService.addOrganisation(organisation);
 
         String name = "theme name";
@@ -60,56 +66,75 @@ public class TestDashboard {
         theme = new Theme(name,description,isCommentaryAllowed,isAddingAdmited,user,organisation,tags);
         theme = contentService.addTheme(theme);
 
-        Session session1 = new AsynchronousSession(true, 60, 4,"nameSession");
-        session1 = sessionService.addSession(session1, theme.getId());
-
-        Session session2 = new AsynchronousSession(true, 60, 4,"nameSession");
-        session2 = sessionService.addSession(session2, theme.getId());
-
-        Session session3 = new AsynchronousSession(true, 60, 4,"nameSession");
-        session3 = sessionService.addSession(session3, theme.getId());
+        List<Card> cardsSession1 = new ArrayList<>();
+        List<Card> cardsSession2 = new ArrayList<>();
+        List<Card> cardsSession3 = new ArrayList<>();
 
         String cardDescription="card1";
-        String url="http://www.google.be";
+        String url="http://www.wired.com/wp-content/uploads/2015/09/google-logo.jpg";
         Card card= new Card(cardDescription,url,theme);
         card=contentService.addCard(card);
-        sessionService.addCardToSession(session1, card);
-        sessionService.addCardToSession(session2, card);
-        sessionService.addCardToSession(session3, card);
+        cardsSession1.add(card);
+        cardsSession2.add(card);
+        cardsSession3.add(card);
 
         cardDescription="card2";
-        url = "http://www.google.be";
+        url = "http://www.wired.com/wp-content/uploads/2015/09/google-logo.jpg";
         card = new Card(cardDescription, url, theme);
         card = contentService.addCard(card);
-        sessionService.addCardToSession(session1, card);
-        sessionService.addCardToSession(session2, card);
+        cardsSession1.add(card);
+        cardsSession2.add(card);
 
         cardDescription="card3";
-        url = "http://www.google.be";
+        url = "http://www.wired.com/wp-content/uploads/2015/09/google-logo.jpg";
         card = new Card(cardDescription, url, theme);
         card = contentService.addCard(card);
-        sessionService.addCardToSession(session1, card);
-        sessionService.addCardToSession(session3, card);*/
+        cardsSession1.add(card);
+        cardsSession3.add(card);
+
+        List<String> usernames = new ArrayList<>();
+        usernames.add(user.getUsername());
+        usernames.add(user2.getUsername());
+        usernames.add(user3.getUsername());
+
+        Session session1 = new AsynchronousSession(user.getId(), false, 60, 4, "sessionName1");
+        session1 = sessionService.addSession(session1, theme.getId(), cardsSession1, usernames);
+
+        Session session2 = new AsynchronousSession(user.getId(), false, 60, 4, "sessionName2");
+        session2 = sessionService.addSession(session2, theme.getId(), cardsSession2, usernames);
+
+        Session session3 = new AsynchronousSession(user.getId(), false, 60, 4, "sessionName3");
+        session3 = sessionService.addSession(session3, theme.getId(), cardsSession3, usernames);
     }
 
     @After
     public void tearDown(){
-      //  userService.deleteOrganisation(organisation.getId());
-        //userService.deleteUser(user.getId());
+        userService.deleteOrganisation(organisation.getId());
+        userService.deleteUser(user.getId());
     }
 
     @Test
     public void testGetMostFrequentCardCombinations() {
-        //List<Set<CardSession>> frequentItemsetList = contentService.findMostFrequentCardCombinations(theme.getThemeId());
-    /* TODO: Geeft nu nog steeds op een willekeurige volgorde terug?
+        List<Set<CardSession>> frequentItemsetList = contentService.findMostFrequentCardCombinations(theme.getThemeId());
+
+        assertEquals("The amount of combinations must be correct", 6, frequentItemsetList.size());
+
+        for(int i = 0; i < frequentItemsetList.size()-1; i++) {
+            assertEquals("The amount of cards in the combination must be correct", 2, frequentItemsetList.get(i).size());
+        }
+
+        assertEquals("The amount of cards in the combination must be correct", 3, frequentItemsetList.get(5).size());
+
+        //TODO: Geeft nu nog steeds op een willekeurige volgorde terug?
+       /*
         String frequentItemsetListString = new ArrayList<>(frequentItemsetList.get(0)).toString();
         assertEquals("The 1st frequent itemsetlist must be correct", "[card3, card1]", frequentItemsetListString);
 
         frequentItemsetListString = new ArrayList<>(frequentItemsetList.get(1)).toString();
-        assertEquals("The 2nd frequent itemsetlist must be correct", "[card1, card1]", frequentItemsetListString);
+        assertEquals("The 2nd frequent itemsetlist must be correct", "[card3, card2]", frequentItemsetListString);
 
         frequentItemsetListString = new ArrayList<>(frequentItemsetList.get(2)).toString();
-        assertEquals("The 3rd frequent itemsetlist must be correct", "[card2, card3]", frequentItemsetListString);
+        assertEquals("The 3rd frequent itemsetlist must be correct", "[card1, card3]", frequentItemsetListString);
 
         frequentItemsetListString = new ArrayList<>(frequentItemsetList.get(3)).toString();
         assertEquals("The 4th frequent itemsetlist must be correct", "[card1, card2]", frequentItemsetListString);
