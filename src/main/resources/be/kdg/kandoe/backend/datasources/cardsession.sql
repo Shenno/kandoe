2214117a5141f7b9e8efb044bb2328d9f9caf5fb
admin <shenno.w@gmail.com>
2016-03-20 19:44:33 +0100

    Session seed

--- src/main/resources/be/kdg/kandoe/backend/datasources/cardsession.sql
+++ src/main/resources/be/kdg/kandoe/backend/datasources/cardsession.sql
@@ -1,10 +1,10 @@
 INSERT INTO CardSession (CardSessionId, CardText, DistanceToCenter, Image, session_id)
 VALUES (1, 'Rode wijn', 5, 'http://www.wijnalbum.nl/Wijnweetjes/fotoos/Glas%20rode%20wijn.gif', 1);
 INSERT INTO CardSession (CardSessionId, CardText, DistanceToCenter, Image, session_id)
-VALUES (2, 'Witte wijn', 5, 'http://www.wijnalbum.nl/Wijnweetjes/fotoos/Glas%20rode%20wijn.gif', 1);
+VALUES (2, 'Witte wijn', 5, 'http://www.ingredienten.nl/images/ingredienten/witte-wijn-large.jpg', 1);
 INSERT INTO CardSession (CardSessionId, CardText, DistanceToCenter, Image, session_id)
-VALUES (3, 'Bier', 5, 'http://www.wijnalbum.nl/Wijnweetjes/fotoos/Glas%20rode%20wijn.gif', 1);
+VALUES (3, 'Bier', 5, 'http://www.plasticsoupfoundation.org/wp-content/uploads/2015/03/Bier-erfunden.jpg', 1);
 INSERT INTO CardSession (CardSessionId, CardText, DistanceToCenter, Image, session_id)
-VALUES (4, 'Jagermeister', 5, 'http://www.wijnalbum.nl/Wijnweetjes/fotoos/Glas%20rode%20wijn.gif', 1);
+VALUES (4, 'Jagermeister', 5, 'http://www.supermarktaanbiedingen.com/Modules/Product/images/2015/2/93014-11-66.jpg', 1);
 INSERT INTO CardSession (CardSessionId, CardText, DistanceToCenter, Image, session_id)
-VALUES (5, 'Fristi', 5, 'http://www.wijnalbum.nl/Wijnweetjes/fotoos/Glas%20rode%20wijn.gif', 1);
+VALUES (5, 'Fristi', 5, 'https://www.hollandforyou.com/images/stories/virtuemart/product/resized/Fristi_Rood_Frui_4fc5d02d14d98_225x225.jpg', 1);
