5abe609622abd4c4a287589e7aad43af70162b85
ellendm <ellendm1991@hotmail.com>
2016-03-24 13:37:00 +0100

    - layout

--- src/main/webapp/app/components/home.component.js
+++ src/main/webapp/app/components/home.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', "angular2/common"], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/router', "angular2/common", "../service/sessionService", "../service/userService"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,25 +10,61 @@ System.register(['angular2/core', "angular2/common"], function(exports_1, contex
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, common_1, common_2;
+    var core_1, router_1, common_1, common_2, sessionService_1, userService_1;
     var HomeComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
             function (common_1_1) {
                 common_1 = common_1_1;
                 common_2 = common_1_1;
+            },
+            function (sessionService_1_1) {
+                sessionService_1 = sessionService_1_1;
+            },
+            function (userService_1_1) {
+                userService_1 = userService_1_1;
             }],
         execute: function() {
             /**
              * Component for Home page
              */
             HomeComponent = (function () {
-                function HomeComponent() {
+                function HomeComponent(userService, sessionService, routeParam, router) {
+                    var _this = this;
+                    this.userId = 0;
+                    this.sessions = [];
                     document.title = 'Kandoe';
+                    this.router = router;
+                    this.userService = userService;
+                    this.sessionService = sessionService;
+                    this.userService.getMyDetails().subscribe(function (user) {
+                        _this.userId = user.id;
+                    });
+                    this.sessionService.getSessionsByUserId(this.userId).subscribe(function (sessions) {
+                        _this.sessions = sessions;
+                        document.title = 'Jouw sessies';
+                        var index = 0;
+                        for (index; index < sessions.length; index++) {
+                            _this.getUser(index);
+                        }
+                    });
                 }
+                HomeComponent.prototype.getUser = function (id) {
+                    var _this = this;
+                    var i = id;
+                    this.userService.getUserById(this.sessions[i].currentUser).subscribe(function (user) {
+                        _this.sessions[i].currentUserName = user.username;
+                    });
+                };
+                HomeComponent.prototype.onClickSession = function (id) {
+                    this.router.navigate(['/Session', { sessionId: id }]);
+                };
                 HomeComponent = __decorate([
                     core_1.Component({
                         selector: 'home',
@@ -36,7 +72,7 @@ System.register(['angular2/core', "angular2/common"], function(exports_1, contex
                         encapsulation: core_1.ViewEncapsulation.None,
                         directives: [common_1.CORE_DIRECTIVES, common_2.FORM_DIRECTIVES]
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [userService_1.UserService, sessionService_1.SessionService, router_1.RouteParams, router_1.Router])
                 ], HomeComponent);
                 return HomeComponent;
             }());
--- src/main/webapp/app/components/home.component.ts
+++ src/main/webapp/app/components/home.component.ts
@@ -1,7 +1,11 @@
 import {Component,  ViewEncapsulation} from 'angular2/core';
-import {Router} from 'angular2/router';
+import {Router, RouteParams} from 'angular2/router';
 import {CORE_DIRECTIVES} from "angular2/common";
 import {FORM_DIRECTIVES} from "angular2/common";
+import {User} from "../entity/user";
+import {SessionActive} from "../entity/sessionActive";
+import {SessionService} from "../service/sessionService";
+import {UserService} from "../service/userService";
 
 /**
  * Component for Home page
@@ -14,7 +18,36 @@ directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 
 export class HomeComponent {
-    public constructor() {
+    private router:Router;
+    private userService:UserService;
+    private sessionService:SessionService;
+    private userId = 0;
+    private sessions:SessionActive[] = [];
+
+    public constructor(userService:UserService,sessionService:SessionService, routeParam:RouteParams, router:Router) {
         document.title = 'Kandoe';
+        this.router = router;
+        this.userService = userService;
+        this.sessionService = sessionService;
+        this.userService.getMyDetails().subscribe((user:User) => {
+            this.userId = user.id;
+        });
+        this.sessionService.getSessionsByUserId(this.userId).subscribe((sessions:SessionActive[]) => {
+            this.sessions = sessions;
+            document.title = 'Jouw sessies';
+            var index = 0;
+            for (index; index < sessions.length; index++) {
+                this.getUser(index);
+            }
+        });
+    }
+    public getUser(id:number):void{
+        var i = id;
+        this.userService.getUserById(this.sessions[i].currentUser).subscribe((user:User) => {
+            this.sessions[i].currentUserName = user.username;
+        });
+    }
+    public onClickSession(id:string):void{
+        this.router.navigate(['/Session',{sessionId:id}]);
     }
 }
