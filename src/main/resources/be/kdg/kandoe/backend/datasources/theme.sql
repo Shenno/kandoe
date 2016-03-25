e74491cc4f8d5f1e24b360a43da73425fddd2569
Blaperile <Devi.Bral@telenet.be>
2016-03-25 11:30:14 +0100

    Default data seeden

--- src/main/resources/be/kdg/kandoe/backend/datasources/theme.sql
+++ src/main/resources/be/kdg/kandoe/backend/datasources/theme.sql
@@ -1,4 +1,4 @@
 INSERT INTO Theme (ThemeName, Description, CommentaryAllowed, AddingAdmited, organisation_id)
-VALUES ('Drankjes', 'Lekker drankje!', FALSE, FALSE, 1);
+VALUES ('Drankjes', 'Wat is jouw favoriete drankje?', FALSE, FALSE, 1);
 INSERT INTO Theme (ThemeName, Description, CommentaryAllowed, AddingAdmited, organisation_id)
-VALUES ('Volle cirkel', 'Test', FALSE, FALSE, 1);
+VALUES ('Sporten', 'Welke sport is de beste?', FALSE, FALSE, 1);
--- src/main/resources/be/kdg/kandoe/backend/datasources/user.sql
+++ src/main/resources/be/kdg/kandoe/backend/datasources/user.sql
@@ -1,10 +1,10 @@
-INSERT INTO "User" ( FirstName, Username, Password)
-VALUES ( 'Clarence','clarence.ho@gmail.com', '$2a$10$GaPeuHGVAtxkksKyHCkQceOT5blhRfeGcmwoER8MeMeX8f65.uf0G');
-INSERT INTO "User" ( FirstName, Username, Password)
-VALUES ( 'Scott','scott.tiger@live.com', '$2a$10$GaPeuHGVAtxkksKyHCkQceOT5blhRfeGcmwoER8MeMeX8f65.uf0G');
-INSERT INTO "User" ( FirstName, Username, Password)
-VALUES ( 'John','john.smith@live.com', '$2a$10$OtWajBubGGHRKO8FdCRIMOdG/pgvTszv7ZegTXdTWTd9uPf4Dqab6');
-INSERT INTO "User" ( FirstName, Username, Password)
-VALUES ( 'Peter','peter.jackson@hotmail.com', '$2a$10$QdDYCq5UnGtX58bAMCB0bOKY07RLcOmhQcMER7RL8Kk0CBow9AkP.');
-INSERT INTO "User" ( FirstName, Username, Password)
-VALUES ( 'Jacky','jacky.chan@telenet.com', '$2a$10$DQdDz9GfeNspdEFWPArJEOJ5004vX4SyzWr3Ewg0DE7YxVkkh8mRe');
+INSERT INTO "User" ( FirstName, LastName, Username, Password)
+VALUES ( 'Clarence', 'Ho', 'clarence.ho@gmail.com', '$2a$10$GaPeuHGVAtxkksKyHCkQceOT5blhRfeGcmwoER8MeMeX8f65.uf0G');
+INSERT INTO "User" ( FirstName, LastName, Username, Password)
+VALUES ( 'Scott', 'Tiger', 'scott.tiger@live.com', '$2a$10$GaPeuHGVAtxkksKyHCkQceOT5blhRfeGcmwoER8MeMeX8f65.uf0G');
+INSERT INTO "User" ( FirstName, LastName, Username, Password)
+VALUES ( 'John', 'Smith', 'john.smith@live.com', '$2a$10$OtWajBubGGHRKO8FdCRIMOdG/pgvTszv7ZegTXdTWTd9uPf4Dqab6');
+INSERT INTO "User" ( FirstName, LastName, Username, Password)
+VALUES ( 'Peter', 'Jackson', 'peter.jackson@hotmail.com', '$2a$10$QdDYCq5UnGtX58bAMCB0bOKY07RLcOmhQcMER7RL8Kk0CBow9AkP.');
+INSERT INTO "User" ( FirstName, LastName, Username, Password)
+VALUES ( 'Jacky', 'Chan', 'jacky.chan@telenet.com', '$2a$10$DQdDz9GfeNspdEFWPArJEOJ5004vX4SyzWr3Ewg0DE7YxVkkh8mRe');
